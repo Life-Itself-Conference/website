ecf698720e951daf672a46f10de97e3a3e7c4cb4
Corey Larson <coreyleelarson@gmail.com>
2023-03-08 16:43:27 -0600

    Removes multiple h1 tags.

--- src/components/atoms/Title/Title.css.ts
+++ src/components/atoms/Title/Title.css.ts
@@ -2,28 +2,36 @@ import { globalStyle, style, styleVariants } from '@vanilla-extract/css';
 import { queries, theme } from '../../../theme.css';
 
 export const title = style({
+  color: theme.colors.white,
   margin: 0,
   marginBottom: theme.spacing.large,
+  position: 'relative',
+  textAlign: 'center',
+  textTransform: 'uppercase',
+  ':before': {
+    backgroundColor: theme.colors.red,
+    content: '',
+    display: 'block',
+    height: 1,
+    position: 'absolute',
+    top: '50%',
+    translate: '0 -50%',
+    width: '100%',
+    zIndex: -1,
+  },
 });
 
 export const tag = styleVariants({
   h1: {
-    color: theme.colors.white,
     fontSize: theme.fontSizes.xlarge,
-    position: 'relative',
-    textAlign: 'center',
-    textTransform: 'uppercase',
-    ':before': {
-      backgroundColor: theme.colors.red,
-      content: '',
-      display: 'block',
-      height: 1,
-      position: 'absolute',
-      top: '50%',
-      translate: '0 -50%',
-      width: '100%',
-      zIndex: -1,
+    '@media': {
+      [queries.large]: {
+        fontSize: theme.fontSizes.xxlarge,
+      },
     },
+  },
+  h2: {
+    fontSize: theme.fontSizes.large,
     '@media': {
       [queries.large]: {
         fontSize: theme.fontSizes.xxlarge,
@@ -32,7 +40,7 @@ export const tag = styleVariants({
   },
 });
 
-globalStyle(`${tag.h1} span`, {
+globalStyle(`${title} span`, {
   backgroundColor: theme.colors.black,
   display: 'inline-block',
   paddingInline: theme.spacing.large,
--- src/components/molecules/ContentSection/index.tsx
+++ src/components/molecules/ContentSection/index.tsx
@@ -8,7 +8,7 @@ import classNames from 'classnames';
 export const ContentSection: ParentComponent<ContentSectionProps> = (props) => (
   <section class={classNames(styles.section, props.class)} id={props.id}>
     <header>
-      <Title tag="h1">{props.title}</Title>
+      <Title tag={props.isHero ? 'h1' : 'h2'}>{props.title}</Title>
       <Container size={props.size}>
         {props.subtitle && <p>{props.subtitle}</p>}
       </Container>
@@ -20,6 +20,7 @@ export const ContentSection: ParentComponent<ContentSectionProps> = (props) => (
 export interface ContentSectionProps {
   class?: string;
   id?: string;
+  isHero?: boolean;
   size?: ContainerProps['size'];
   subtitle?: string;
   title: string;
