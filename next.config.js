ad258e1331c9469edb48a6978d88e3aae46ca311
Corey Larson <coreyleelarson@gmail.com>
2023-08-22 12:19:39 -0500

    Resets back to standalone for Azure.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
+  output: "standalone",
 };
 
 module.exports = withVanillaExtract(nextConfig);
