ca64fee0f51330071ea4a74693b9ad523d1a1520
Corey Larson <coreyleelarson@gmail.com>
2023-08-22 11:57:35 -0500

    Removes standalone.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  output: "standalone",
 };
 
 module.exports = withVanillaExtract(nextConfig);
