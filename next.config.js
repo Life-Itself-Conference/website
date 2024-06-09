f4005a007f29b402b5bdeacdb844159680a9588f
Corey Larson <coreyleelarson@gmail.com>
2024-06-08 22:08:37 -0500

    Upgrades next.

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: { unoptimized: true },
 };
 
